36a2448284fbc9be0662e5472e3daf3f5eaa0d08
robertbradl <93824562+robertbradl@users.noreply.github.com>
2023-08-29 08:42:34 +0200

    Initial Commit

---
+++ Memoir/MemoirApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirApp.swift
+//  Memoir
+//
+//  Created by Robert Leonard Bradl on 29.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MemoirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
